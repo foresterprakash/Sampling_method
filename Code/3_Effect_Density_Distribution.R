b065f83a19dd28e31d1c32574120b1e6a43d3bad
shivakhanal <khanalshiva1@gmail.com>
2024-03-02 11:19:11 +0545

    Prakash, THis is for third objective. Please recheck this. While comparing the outputs from the second code, we can plot the total vs different CCSP and make size of points by gini coeffient.

---
+++ Code/3_Effect_Density_Distribution.R
@@ -0,0 +1,48 @@
+
+
+# We can use Gini coefficient to evaluate the size heterogenity
+# example papers: https://link.springer.com/article/10.1007/s11104-023-05874-2#Sec2
+# https://iforest.sisef.org/contents/?id=ifor4096-015
+
+
+# if dat is plot level inventory data
+
+# Function to calculate Gini coefficient
+gini_coefficient <- function(data) {
+  # Sort data in ascending order
+  data <- sort(data)
+  
+  # Calculate Lorenz curve
+  n <- length(data)
+  lorenz_y <- cumsum(data) / sum(data)
+  lorenz_x <- seq(1, n) / n
+  
+  # Calculate Gini coefficient using trapezoidal rule
+  A <- sum(lorenz_x * (lorenz_y - (1:n) / n))
+  G <- 1 - 2 * A
+  
+  return(G)
+}
+
+# Calculate summary statistics by plot using group_by
+plot_summary <- dat_analysed %>%
+  group_by(plot_id) %>%
+  summarise(
+    n_trees = n(),
+    mean_dbh = mean(dbh),
+    total_agb = sum(total_biomass_ton_ha)/1000,
+    gini_dbh = gini_coefficient(dbh)
+  )
+
+# Print the summary table
+#with(plot_summary, plot(gini_dbh, total_agb))
+#with(plot_summary, abline(0, 1))
+
+library(ggplot2)
+ggplot(plot_summary, aes(gini_dbh, total_agb)) +
+  geom_point() +
+  #geom_abline(intercept = 0, slope = 1, color = "red", linewidth = 0.5) +
+  geom_smooth() +
+  xlab("Gini Index (dbh)") +
+  ylab("Total Above-Ground Biomass")
+
